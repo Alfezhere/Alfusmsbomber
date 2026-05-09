8579e29ba132b24e792a3936987e73cc1bad5e66
Alfezhere <minesign001@gmail.com>
2026-05-09 11:09:50 +0530

    Update smsbomberbot.py

--- smsbomberbot.py
+++ smsbomberbot.py
@@ -10,6 +10,20 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import logging
+from flask import Flask
+from threading import Thread
+import os
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot Running"
+
+def run():
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
+
+Thread(target=run).start()
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
